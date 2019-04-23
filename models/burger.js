b514d1041db14074db6e8cc1b602a801be1e42a6
Chesterfield <boyce.davidw@gmail.com>
2019-04-22 20:23:20 -0400

    full functionality

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,29 @@
 const ORM = require('../config/orm.js');
+
+var burger = {
+    selectAll: function(cb) {
+        ORM.selectAll("burgers", function(res) {
+          cb(res);
+        });
+      },
+
+    insertOne: function(cols, vals, cb) {
+      ORM.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+
+    updateOne: function(objColVals, condition, cb) {
+      ORM.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+
+    delete: function(condition, cb) {
+      ORM.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+}
+
+module.exports = burger;
